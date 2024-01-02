3cf25abb922bbf00fba4f1372f4d65f230660d2a
ermias kidane <ermiaskidanegebre@gmail.com>
2024-01-02 19:24:17 +0000

    initial commit

---
+++ actions/register.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import bcrypt from "bcrypt"
+import { RegisterSchema } from "@/schemas";
+import * as z from "zod";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  // console.log(values)
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if(!validatedFields.success) {
+    return { error:  "Invalid fields!"}
+  }
+
+  const { email, password, name} = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  // const existingUser = await db.user.findUnique({
+  //   where: {
+  //     email
+  //   }
+  // })
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return  {error: "Email already in use"}
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email, 
+      password: hashedPassword
+    }
+  })
+
+  // TODO: send verification token email
+  return { success: "User created"};
+}
